7c2303dab51fffb44abde31c23b62d666c9c27ae
Pszczoła <dpszczola@multiconsult.no>
2021-12-13 16:19:21 +0100

    improve circle/ellipse feature

--- src/js/tools/circle.js
+++ src/js/tools/circle.js
@@ -9,31 +9,39 @@ export default class Circle extends Tool {
 		this._startY = 0;
 	}
 
+
 	drawFigure(x, y, ctx, toolProp) {
+        ctx.save();
 		ctx.lineWidth = toolProp.size;
 		ctx.lineCap = "round";
 		ctx.strokeStyle = toolProp.color;
 		ctx.beginPath();
-		// ctx.strokeRect(this._startX, this._startY, x - this._startX, y - this._startY);
-        ctx.arc(this._startX, this._startY, x - this._startX, 0, 2 * Math.PI);
+        const i = Math.min(this._startX, x);
+        let r = Math.min(this._startY, y);
+        let n = Math.max(this._startX, x);
+        let a = Math.max(this._startY, y);
+        let h = i + (n - i) / 2;
+        let c = r + (a - r) / 2;
+        let l = (n - i) / 2;
+        let d = (a - r) / 2;
+        ctx.ellipse(h, c, l, d, 0, 0, 2 * Math.PI);
         ctx.stroke();
-        ctx.closePath();
 		ctx.restore();
 	}
 
-	    //draw pointer at cursor's place
-        drawPointer(x, y, ctx, toolProp) {
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-            ctx.save();
-            ctx.lineWidth = 2;
-            ctx.lineCap = "round";
-            ctx.strokeStyle = toolProp.color;
-            ctx.globalAlpha = 1;
-            ctx.beginPath();
-            ctx.arc(x, y, toolProp.size/2-1, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.restore();
+	 //draw pointer at cursor's place
+     drawPointer(x, y, ctx, toolProp) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.save();
+        ctx.lineWidth = 2;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = toolProp.color;
+        ctx.globalAlpha = 1;
+        ctx.beginPath();
+        ctx.arc(x, y, toolProp.size/2-1, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.restore();
         }
 
 	onMouseMove(x, y, ctx1, ctx2, toolProp) {
